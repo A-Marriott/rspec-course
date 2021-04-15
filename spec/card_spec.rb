9b55758355d16df2b31b6433a08803cd386460e6
Alex Marriott <marriottaog@gmail.com>
2021-04-15 11:16:36 +0100

    first time using let method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,13 @@
 require 'card'
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 
   it 'has a value' do
-    expect(@card.value).to eq('Ace')
+    expect(card.value).to eq('Ace')
   end
 end
