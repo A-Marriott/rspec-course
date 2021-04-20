dad9170a28de81286a9ca18735f133c11d06e279
Alex Marriott <marriottaog@gmail.com>
2021-04-20 10:54:18 +0100

    explicit subject assignment

--- spec/10_explicit_subject_spec.rb
+++ spec/10_explicit_subject_spec.rb
@@ -11,3 +11,19 @@ end
 # Two things we do here:
 # 1. We can alter subject before executing tests to whatever we want it to be
 # 2. We rename the subject variable to whatever we pass as an argument (in this case :my_hash)
+
+describe Array do
+  subject(:sally) do
+    [1, 2]
+  end
+
+  it 'should have a length of two' do
+    expect(sally.length).to eq(2)
+    sally.pop
+    expect(sally.length).to eq(1)
+  end
+
+  it 'should still have a length of two' do
+    expect(sally.length).to eq(2)
+  end
+end
