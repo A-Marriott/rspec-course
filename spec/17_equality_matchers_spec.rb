9b89109d7e2ff058e2b0fe9a01bf3657d0103e76
Alex Marriott <marriottaog@gmail.com>
2021-04-20 11:59:16 +0100

    equal/be

--- spec/17_equality_matchers_spec.rb
+++ spec/17_equality_matchers_spec.rb
@@ -13,4 +13,27 @@ describe 'equality matchers' do
       expect(a).not_to eql(b)
     end
   end
+
+  describe 'equal and be matcher' do
+    let(:c) { [1, 2, 3] }
+    let(:d) { [1, 2, 3] }
+    let(:e) { c }
+
+    it 'cares about object identity' do
+      expect(c).not_to equal(d)
+      expect(c).to be(e)
+    end
+  end
 end
+
+
+# when we check equality, we check that two things are equal in design. That they're the same object in memory
+# e.g. we build two houses identically in design. They're equal in design completely, but they're not the same object.
+# the variable name is like the address, and the object is the design.
+# c and d are equal, but not identical. They point to two different places in memory
+
+# be == equal. be kind of makes more sense lexically.
+
+# 1. unstrict equality
+# 2. strict equality
+# 3. identity
