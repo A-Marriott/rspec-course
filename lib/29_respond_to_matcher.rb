543aa98a22a0fec60eccbae87d9ab28e8f069420
Alex Marriott <marriottaog@gmail.com>
2021-04-27 15:23:58 +0100

    respond to

---
+++ lib/29_respond_to_matcher.rb
@@ -0,0 +1,13 @@
+class HotChocolate
+  def drink
+    'Delicious'
+  end
+
+  def discard
+    'Bye'
+  end
+
+  def purchase(number)
+    "I have #{number} more drinks"
+  end
+end
--- spec/29_respond_to_matcher_spec.rb
+++ spec/29_respond_to_matcher_spec.rb
@@ -1,1 +1,13 @@
 require '29_respond_to_matcher'
+
+describe HotChocolate do
+  it 'confirms and object responds to a method' do
+    expect(subject).to respond_to(:drink, :discard, :purchase)
+  end
+
+  it 'confirms that object can respond to a method with arguments' do
+    expect(subject).to respond_to(:purchase).with(1).arguments
+  end
+end
+
+# Concept behind this matcher is centred around polymorphism. We're literally just checking that a given object has access to a given method, not about what happens within the method
